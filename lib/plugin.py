4a10bd58822ed447260b5875cc174b6170d3473b
Malek <lekmalek@gmail.com>
2020-05-05 21:04:11 +0200

    redesign - first pass

--- lib/plugin.py
+++ lib/plugin.py
@@ -4,15 +4,15 @@
 from __future__ import absolute_import, division, unicode_literals
 
 
+import sys
+
 from six import wraps
-from kodi_six import xbmc, xbmcplugin
+from kodi_six import xbmcplugin
 from inputstreamhelper import Helper
 
-from .utils import parse_query, more_item
-from .utils import localized_string, search_dialog
-from .invidious import _trending_styles_, _search_styles_
-from .invidious.api import service
-from .invidious.objects import Folder, Folders, Home
+from client import client
+from objects import Home, Folders, Folder, _trending_styles_, _search_styles_
+from utils import parseQuery, getMoreItem, searchDialog, localizedString, log
 
 
 def action(category=0):
@@ -35,12 +35,15 @@ def action(category=0):
     return decorator
 
 
+# ------------------------------------------------------------------------------
+# Dispatcher
+# ------------------------------------------------------------------------------
+
 class Dispatcher(object):
 
     def __init__(self, url, handle):
         self.url = url
         self.handle = handle
-
 
     # utils --------------------------------------------------------------------
 
@@ -60,13 +63,13 @@ class Dispatcher(object):
                 kwargs["continuation"] = continuation
             else:
                 kwargs["page"] = int(kwargs.get("page", 1)) + 1
-            self.addItem(more_item(self.url, action=self.action, **kwargs))
+            self.addItem(getMoreItem(self.url, action=self.action, **kwargs))
         if items.content:
             xbmcplugin.setContent(self.handle, items.content)
         if items.category:
             if self.category:
                 self.setCategory(" / ".join((items.category,
-                                             localized_string(self.category))))
+                                             localizedString(self.category))))
             else:
                 self.setCategory(items.category)
         return True
@@ -77,40 +80,39 @@ class Dispatcher(object):
 
     def endDirectory(self, success):
         if success and self.category:
-            self.setCategory(localized_string(self.category))
+            self.setCategory(localizedString(self.category))
         xbmcplugin.endOfDirectory(self.handle, success)
 
     def getSubfolders(self, _type, styles):
         return ({"type": _type, "style": style} for style in styles)
 
-
     # actions ------------------------------------------------------------------
 
-    def play(self, item, manifest_type, mime_type=None):
-        if not Helper(manifest_type).check_inputstream():
+    def play(self, item, manifest, mime=None):
+        if not Helper(manifest).check_inputstream():
             return False
         item.setProperty("inputstreamaddon", "inputstream.adaptive")
-        item.setProperty("inputstream.adaptive.manifest_type", manifest_type)
-        if mime_type:
-            item.setMimeType(mime_type)
+        item.setProperty("inputstream.adaptive.manifest_type", manifest)
+        if mime:
+            item.setMimeType(mime)
             item.setContentLookup(False)
         xbmcplugin.setResolvedUrl(self.handle, True, item)
         return True
 
     @action()
     def video(self, **kwargs):
-        item = service.video(**kwargs)
-        return self.play(*item) if item else False
+        args = client.video(**kwargs)
+        return self.play(*args) if args else False
 
     @action()
     def channel(self, **kwargs):
         if int(kwargs.get("page", 1)) == 1:
             self.addItem(Folder({"type": "playlists"}).item(self.url, **kwargs))
-        return self.addItems(service.channel(**kwargs), "video", **kwargs)
+        return self.addItems(client.channel(**kwargs), "video", **kwargs)
 
     @action()
     def playlist(self, **kwargs):
-        return self.addItems(service.playlist(**kwargs), "video", **kwargs)
+        return self.addItems(client.playlist(**kwargs), "video", **kwargs)
 
     @action()
     def home(self, **kwargs):
@@ -118,45 +120,43 @@ class Dispatcher(object):
 
     @action(30005)
     def playlists(self, **kwargs):
-        return self.addItems(service.playlists(**kwargs), "playlist", **kwargs)
+        return self.addItems(client.playlists(**kwargs), "playlist", **kwargs)
 
     @action(30007)
     def top(self, **kwargs):
-        return self.addItems(service.top(**kwargs), "video")
+        return self.addItems(client.top(**kwargs), "video")
 
     @action(30008)
     def popular(self, **kwargs):
-        return self.addItems(service.popular(**kwargs), "video")
+        return self.addItems(client.popular(**kwargs), "video")
 
     @action(30009)
     def trending(self, **kwargs):
         if not "type" in kwargs:
             self.addItems(
                 Folders(self.getSubfolders("trending", _trending_styles_)))
-        return self.addItems(service.trending(**kwargs), "video")
-
+        return self.addItems(client.trending(**kwargs), "video")
 
     # search -------------------------------------------------------------------
 
     @action(30002)
     def search(self, **kwargs):
         if not "type" in kwargs:
-            service.queries.clear()
+            client.queries.clear()
             return self.addItems(
                 Folders(self.getSubfolders("search", _search_styles_)))
         q = kwargs.pop("q", "")
         if not q:
             try:
-                q, kwargs = service.queries.pop()
+                q, kwargs = client.queries.pop()
             except IndexError:
-                q = search_dialog()
+                q = searchDialog()
                 if q:
-                    service.queries.append((q, kwargs))
+                    client.queries.append((q, kwargs))
         if q:
             return self.addItems(
-                service.search(q, **kwargs), kwargs["type"], q=q, **kwargs)
+                client.search(q, **kwargs), kwargs["type"], q=q, **kwargs)
         return False
-
 
     # dispatch -----------------------------------------------------------------
 
@@ -167,6 +167,13 @@ class Dispatcher(object):
         return action(**kwargs)
 
 
+# __main__ ---------------------------------------------------------------------
+
 def dispatch(url, handle, query, *args):
-    Dispatcher(url, int(handle)).dispatch(**parse_query(query))
+    Dispatcher(url, int(handle)).dispatch(**parseQuery(query))
+
+
+if __name__ == "__main__":
+
+    dispatch(*sys.argv)
 
--- lib/utils.py
+++ lib/utils.py
@@ -11,67 +11,56 @@ from six.moves.urllib.parse import parse_qsl, urlencode
 from kodi_six import xbmc, xbmcaddon, xbmcgui
 
 
-addon = xbmcaddon.Addon()
-addonPath = xbmc.translatePath(addon.getAddonInfo("path"))
-addonId = addon.getAddonInfo("id")
+_addon_ = xbmcaddon.Addon()
+_addon_id_ = _addon_.getAddonInfo("id")
+_addon_path_ = xbmc.translatePath(_addon_.getAddonInfo("path"))
+_addon_icon_ = xbmc.translatePath(_addon_.getAddonInfo("icon"))
 
-dialog = xbmcgui.Dialog()
+_dialog_ = xbmcgui.Dialog()
 
 
-def parse_query(query):
+def getWindowId():
+    return xbmcgui.getCurrentWindowId()
+
+def getAddonId():
+    return _addon_id_
+
+def getAddonPath():
+    return _addon_path_
+
+def getAddonIcon():
+    return _addon_icon_
+
+
+def parseQuery(query):
     if query.startswith("?"):
         query = query[1:]
     return dict(parse_qsl(query))
 
 
-def build_url(*args, **kwargs):
+def buildUrl(*args, **kwargs):
     params = {k: v.encode("utf-8") if isinstance(v, text_type) else v
               for k, v in iteritems(kwargs)}
     return "?".join(("/".join(args), urlencode(params)))
 
 
-def localized_string(id):
+def localizedString(id):
     if id < 30000:
         return xbmc.getLocalizedString(id)
-    return addon.getLocalizedString(id)
+    return _addon_.getLocalizedString(id)
 
 
-def get_media_path(*args):
-    return join(addonPath, "resources", "media", *args)
+def getMediaPath(*args):
+    return join(_addon_path_, "resources", "media", *args)
 
-def get_icon(name):
-    return get_media_path("{}.png".format(name))
-
-
-def get_window_id():
-    return xbmcgui.getCurrentWindowId()
-
-def get_addon_id():
-    return addonId
-
-def get_addon_path():
-    return addonPath
-
-
-# settings ---------------------------------------------------------------------
-
-_get_settings_ = {
-    bool: "getSettingBool",
-    int: "getSettingInt",
-    float: "getSettingNumber",
-    unicode: "getSettingString"
-}
-
-def get_setting(id, _type=None):
-    if _type is not None:
-        return _type(getattr(addon, _get_settings_.get(_type, "getSetting"))(id))
-    return addon.getSetting(id)
+def getIcon(name):
+    return getMediaPath("{}.png".format(name))
 
 
 # logging ----------------------------------------------------------------------
 
 def log(msg, level=xbmc.LOGNOTICE):
-    xbmc.log("[{}] {}".format(addonId, msg), level=level)
+    xbmc.log("[{}] {}".format(_addon_id_, msg), level=level)
 
 
 def debug(msg):
@@ -80,6 +69,47 @@ def debug(msg):
 
 def warn(msg):
     log(msg, xbmc.LOGWARNING)
+
+
+def error(msg):
+    log(msg, xbmc.LOGERROR)
+
+
+# settings ---------------------------------------------------------------------
+
+_get_settings_ = {
+    bool: _addon_.getSettingBool,
+    int: _addon_.getSettingInt,
+    float: _addon_.getSettingNumber,
+    unicode: _addon_.getSettingString
+}
+
+def getSetting(id, _type=None):
+    if _type is not None:
+        return _type(_get_settings_.get(_type, _addon_.getSetting)(id))
+    return _addon_.getSetting(id)
+
+
+# notify -----------------------------------------------------------------------
+
+iconInfo = xbmcgui.NOTIFICATION_INFO
+iconWarning = xbmcgui.NOTIFICATION_WARNING
+iconError = xbmcgui.NOTIFICATION_ERROR
+
+_addon_heading_ = localizedString(30000)
+
+def notify(message, heading=_addon_heading_, icon=_addon_icon_, time=2000):
+    if isinstance(message, int):
+        message = localizedString(message)
+    _dialog_.notification(heading, message, icon, time)
+
+
+# search -----------------------------------------------------------------------
+
+_search_label_ = localizedString(30002)
+
+def searchDialog():
+    return _dialog_.input(_search_label_)
 
 
 # listitem ---------------------------------------------------------------------
@@ -92,7 +122,7 @@ class ListItem(xbmcgui.ListItem):
             offscreen=True)
 
     def __init__(self, label, path, isFolder=False, infos=None,
-                 streamInfos=None, properties=None, **art):
+                 streamInfos=None, contextMenus=None, **art):
         self.setIsFolder(isFolder)
         self.setIsPlayable(not isFolder)
         self.isFolder = isFolder
@@ -102,8 +132,8 @@ class ListItem(xbmcgui.ListItem):
         if streamInfos:
             for info in iteritems(streamInfos):
                 self.addStreamInfo(*info)
-        if properties:
-            self.setProperties(properties)
+        if contextMenus:
+            self.addContextMenuItems(contextMenus)
         if art:
             self.setArt(art)
 
@@ -114,30 +144,11 @@ class ListItem(xbmcgui.ListItem):
         return self.getPath(), self, self.isFolder
 
 
-_more_label_ = localized_string(30099)
-_more_icon_ = get_icon("more")
+_more_label_ = localizedString(30099)
+_more_icon_ = getIcon("more")
 
-def more_item(url, **kwargs):
+def getMoreItem(url, **kwargs):
     return ListItem(
-        _more_label_,  build_url(url, **kwargs), isFolder=True,
+        _more_label_,  buildUrl(url, **kwargs), isFolder=True,
         infos={"video": {"plot": _more_label_}}, icon=_more_icon_)
-
-
-# search -----------------------------------------------------------------------
-
-_search_label_ = localized_string(30002)
-
-def search_dialog():
-    return dialog.input(_search_label_)
-
-
-# notify -----------------------------------------------------------------------
-
-_notify_heading_ = localized_string(30000)
-
-def notify(message, heading=_notify_heading_, icon=xbmcgui.NOTIFICATION_ERROR,
-           time=2000):
-    if isinstance(message, int):
-        message = localized_string(message)
-    dialog.notification(heading, message, icon, time)
 
